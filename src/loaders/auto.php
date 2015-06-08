0462fdd5a359de5858c4065adf73c6e9346261f2
Sameer Shelavale <sameer@gohachi.com>
2015-06-08 18:28:15 +0530

    Added an auto loader class which loads form definitions from different types of objects/arrays/xml/files by calling respective loader classes.

---
+++ src/loaders/auto.php
@@ -0,0 +1,22 @@
+<?php
+namespace X2Form\Loaders;
+use \X2Form\Helpers\Logger;
+
+class Auto{
+
+    public function load( &$form, $source, $exclude=null ){
+        if( $source instanceof SimpleXMLElement ){
+            return \X2Form\Loaders\Simplexml::load( $form, $source, $exclude );
+        }elseif( is_subclass_of( $source, 'Illuminate\Database\Eloquent\Model' ) ){
+            //passed laravel Model object or classname
+            return \X2Form\Loaders\Eloquent::load( $form, $source, $exclude );
+        }elseif( is_file( $source ) && is_readable( $source ) ){
+            //filename passed
+            return \X2Form\Loaders\Xml::loadFile( $form, $source, $exclude );
+        }elseif( is_string( $source ) && strlen( $source ) > 50 ){
+            //xml is passed
+            return \X2Form\Loaders\Xml::load($form, $source, $exclude );
+        }
+        return Logg( 'Failure', 'The type of source object provided is not supportd.' );
+    }
+};
