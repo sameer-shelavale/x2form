4c3012ed100db33a86c4b278a084127b06888d73
Sameer Shelavale <sameer@gohachi.com>
2015-08-09 07:24:04 +0530

    moved the autoloader code to autoload.php

--- src/form.php
+++ src/form.php
@@ -37,28 +37,6 @@ use X2Form\Loaders;
 use ArrayObject;
 use SimpleXMLElement;
 
-require_once ( ltrim( __DIR__, '/'). '/helpers/dbhelper.php' );
-require_once ( ltrim( __DIR__, '/'). '/helpers/logger.php' );
-
-//Auto load the required php classes
-spl_autoload_register( function($className){
-    $curDir = ltrim( __DIR__, '/');
-    //$cls = array_pop( explode('\\', $className) );
-
-    if( preg_match( '/X2Form\\\Renderers\\\(bootstrap|table|div|jqueryui|table)\\\(.+)$/i', $className, $matches ) ){
-        $cls = ltrim( strtolower( preg_replace( '/([A-Z])/', '_$1',$matches[2])), '_');
-        require_once( $curDir.'/renderers/'. strtolower($matches[1]).'/'.$cls.'.php');
-    }elseif( preg_match( '/X2Form\\\Loaders\\\(.+)$/i', $className, $matches ) ){
-        $cls = ltrim( strtolower( preg_replace( '/([A-Z])/', '_$1',$matches[1])), '_');
-        require_once( $curDir.'/loaders/'. $cls.'.php');
-    }elseif( preg_match( '/X2Form\\\Interfaces\\\(.+)$/i', $className, $matches ) ){
-        $cls = ltrim( strtolower( preg_replace( '/([A-Z])/', '_$1',$matches[1])), '_');
-        require_once( $curDir.'/interfaces/'. $cls.'.php');
-    }elseif( preg_match( '/X2Form\\\(.+)$/i', $className, $matches ) ){
-        $cls = ltrim( strtolower( preg_replace( '/([A-Z])/', '_$1',$matches[1])), '_');
-        require_once( $curDir.'/'.$cls.'.php');
-    }
-});
 
 class Form{
 
