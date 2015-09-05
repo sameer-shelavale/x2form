ae696d15b5bd9b86a91ceb3e892bc1308c7bd02e
Sameer Shelavale <sameer@gohachi.com>
2015-09-05 15:12:01 +0530

    Added a sample showing how to render forms using bootstrap.

--- src/loaders/simplexml.php
+++ src/loaders/simplexml.php
@@ -1,20 +1,4 @@
 <?php
-/*******************************************************************************************************
- * class Logger
- * Logger class is used for logging errors to screen or text file, 
- * It is useful in webservices and APIs to return structured results.
- * 
- * Author : Sameer Shelavale
- * Email  : samiirds@gmail.com, sameer@techrevol.com, sameer@possible.in 
- * Author website: http://techrevol.com, http://possible.in
- * Phone  : +91 9890103122
- * License: AGPL3, You should keep Package name, Class name, Author name, Email and website credits.
- * 			http://www.gnu.org/licenses/agpl-3.0.html
- * 			For other type of licenses please contact the author.
- * PHP Version: Tested on PHP 5.2.2 & 5.3.10
- * Copyrights (C) 2012-2013 Sameer Shelavale
- * Dependencies : class.dbhelper.php, class.logg.php
- *******************************************************************************************************/
 namespace X2Form\Loaders;
 use X2Form\Helpers;
 use ArrayObject;
