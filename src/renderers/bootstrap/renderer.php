9ce4d86a6915796c16a8b11ecbb75f2b8b848663
Sameer Shelavale <sameer@gohachi.com>
2015-12-12 17:11:56 +0530

    Added a support for Group element in templates.

--- src/renderers/bootstrap/renderer.php
+++ src/renderers/bootstrap/renderer.php
@@ -111,10 +111,12 @@ class Renderer extends BasicRenderer implements \X2Form\Interfaces\Renderer{
             }else{
                 if( $element instanceof \X2Form\Collection ){
                     $template = str_replace( "[{$element->name}]", $this->collectionRenderer->render( $element  ), $template );
+                }elseif( $element instanceof \X2Form\Group ){
+                    $template = str_replace( "[{$element->name}]", $this->groupRenderer->render( $element  ), $template );
                 }else{
                     $template = str_replace( "[{$element->name}]", $this->elementRenderer->render( $element  ), $template );
                 }
-                $template = str_replace( "[{$element->name}_label]", $element->label(), $template );
+                $template = str_replace( "[{$element->name}_label]", $this->elementRenderer->makeLabel( $element ), $template );
                 $template = str_replace( "[{$element->name}_description]", $element->description(), $template );
 
             }
